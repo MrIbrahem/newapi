16880ef3fc2aaf3bfeab6d200bda3d70ee5df8ca
ibrahem <ibrahem.al-radaei@outlook.com>
2024-07-18 00:46:20 +0300

    Update r3_token_bot.py

--- super/login_bots/r3_token_bot.py
+++ super/login_bots/r3_token_bot.py
@@ -3,21 +3,30 @@
 from newapi.super.login_bots.r3_token_bot import get_r3_token
 
 Exception:{'login': {'result': 'Failed', 'reason': 'You have made too many recent login attempts. Please wait 5 minutes before trying again.'}}
+WARNING: I:/core/bots/new/newapi/super/login_bots/bot.py:134: UserWarning:
+def get_login_result(): Exception:{'login': {'result': 'Aborted', 'reason': 'Cannot log in when using MediaWiki\\Session\\BotPasswordSessionProvider sessions.'}}
+
+WARNING: I:/core/bots/new/newapi/super/login_bots/bot.py:134: UserWarning:
+def get_login_result(): Exception:{'login': {'result': 'Failed', 'reason': 'You have made too many recent login attempts. Please wait 2 days before trying again.'}}
 
 """
+import os
 from pathlib import Path
 from newapi import printe
 
-# from ratelimiter import RateLimiter
-# limiter = RateLimiter(max_calls=1, period=1)
+import stat
+
+statgroup = stat.S_IRWXU | stat.S_IRWXG
+token_dir = Path(__file__).parent / "r3_tokens"
 # ---
-tocken_dir = Path(__file__).parent / "r3_tokens"
-# ---
-if not tocken_dir.exists():
-    tocken_dir.mkdir()
+if not token_dir.exists():
+    token_dir.mkdir()
     printe.output("<<green>> mkdir:")
-    printe.output(f"tocken_dir:{tocken_dir}")
+    printe.output(f"token_dir:{token_dir}")
     printe.output("<<green>> mkdir:")
+    os.chmod(token_dir, statgroup)
+
+token_tab = {}
 
 
 def get_file_name(lang, family, username):
@@ -27,18 +36,23 @@ def get_file_name(lang, family, username):
     # ---
     username = username.lower().replace(" ", "_").split("@")[0]
     # ---
-    file = tocken_dir / f"{family}_{lang}_{username}.txt"
+    file = token_dir / f"{family}_{lang}_{username}.txt"
     # ---
     return file
 
 
 def dump_r3_token(lang, family, username, r3_token):
     # ---
+    token_tab.setdefault(family, {}).setdefault(lang, {})
+    # ---
+    token_tab[family][lang][username] = r3_token
+    # ---
     file = get_file_name(lang, family, username)
     # ---
     try:
         with open(file, "w") as f:
             f.write(r3_token)
+            os.chmod(str(file), statgroup)
     except Exception as e:
         printe.output(e)
     # ---
@@ -54,13 +68,19 @@ def from_folder(lang, family, username):
     if file.exists():
         with open(file, "r") as f:
             r3_token = f.read()
+    else:
+        file.touch()
+        os.chmod(str(file), statgroup)
     # ---
     return r3_token
 
 
 def get_r3_token(lang, family, username):
     # ---
-    r3_token = from_folder(lang, family, username)
+    r3_token = token_tab.get(family, {}).get(lang, {}).get(username, "")
+    # ---
+    if not r3_token:
+        r3_token = from_folder(lang, family, username)
     # ---
     if not r3_token:
         printe.output(f" <<red>> get_r3_token: <<yellow>> [[{lang}:{family}]] user:{username} <<red>> not found")
