e825e58145f9749ed438bacd1ebce3255c543400
ibrahem <ibrahem.al-radaei@outlook.com>
2024-07-23 23:14:57 +0300

    Update bot.py

--- super/login_bots/bot.py
+++ super/login_bots/bot.py
@@ -285,7 +285,7 @@ class LOGIN_HELPS:
         if "minor" in params and params["minor"] == "":
             params["minor"] = self.Bot_or_himo
 
-        if params["action"] in ["edit", "create", "upload", "delete", "move"] or params["action"].startswith("wd"):
+        if params["action"] in ["edit", "create", "upload", "delete", "move"] or params["action"].startswith("wd") or self.family == "wikidata":
             params["assertuser"] = self.username
 
         return params
